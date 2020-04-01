072c440cb9f9ea4b2129bd2a06a9434a1597b85d
Jeremy L <jlewis218@outlook.com>
2020-04-01 10:34:57 -0500

    Created game logic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let cardOne = 'King';
+let cardTwo = 'Queen';
+let cardThree = 'King';
+let cardFour = 'Queen';
+
+console.log('User has flipped: ' + cardTwo);
+console.log('User has flipped: ' + cardThree);
