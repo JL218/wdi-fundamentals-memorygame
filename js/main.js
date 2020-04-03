c0498eb1674bec7380d08db5052e603d7fd70107
Jeremy L <jlewis218@outlook.com>
2020-04-03 15:01:00 -0500

    Created Game Logic and Organized Code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
-// Create an array which contains the 4 cards;
+// // Create an array which contains the 4 cards;
+// Global Variables
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = [];
-const cardOne = cards[0];
-const cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped: ' + cardOne);
-console.log('User flipped: ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
+// // Function Declarations
+// checking for matching pairs
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!');
   } else {
     alert('Sorry, but this is not a matching pair. Please try again.');
   }
 }
+
+// flipping the cards over
+function flipCard(cardId) {
+  // const cardOne = cards[0];
+  // const cardTwo = cards[2];
+
+  // cardsInPlay.push(cardOne);
+  // cardsInPlay.push(cardTwo);
+
+  // console.log('User flipped: ' + cardOne);
+  // console.log('User flipped: ' + cardTwo);
+  console.log('User flipped: ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2) {
+    // check for a matching pair, as declared in the function above this one;
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
